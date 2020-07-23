b9b1312a2bbbb8d1952828a06df4a787c70ae30b
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-23 21:54:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
     new_cart_item  {
       :item => cart[counter][:item],
       :price => cart[counter][:price],
-      :clearance => cart[counter][:clearance]
+      :clearance => cart[counter][:clearance],
       :count => 1 
       new_cart << new_cart_item
     }
